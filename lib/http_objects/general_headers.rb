b0af1de4b8b49cc13b97761cf6e674b6e192912c
Roger Leite <roger.barreto@gmail.com>
2012-12-12 17:39:38 -0200

    More General Headers

--- lib/http_objects/general_headers.rb
+++ lib/http_objects/general_headers.rb
@@ -2,7 +2,24 @@ module HttpObjects::GeneralHeaders
 
   extend HttpObjects::Tools
 
+  # 14.10 Connection
+  Connection = Header("Connection", HttpObjects::Parameters::BasicRules::Octet)
+
   # 14.18 Date
   Date = Header("Date", HttpObjects::Parameters::DateTime)
+
+  # 14.40 Trailer
+  Trailer = Header("Trailer", HttpObjects::Parameters::BasicRules::Octet)
+
+  # 14.42 Upgrade
+  Upgrade = Header("Upgrade", HttpObjects::Parameters::BasicRules::Octet)
+
+  # 14.45 Via
+  # Pending: implement spec
+  Via = Header("Via", HttpObjects::Parameters::BasicRules::Octet)
+
+  # 14.46 Warning
+  # Pending: implement spec
+  Warning = Header("Warning", HttpObjects::Parameters::BasicRules::Octet)
 
 end
---
+++ test/http_objects/general_headers_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+
+include HttpObjects::GeneralHeaders
+
+describe Connection do
+  subject { Connection.parse("close") }
+  it "should be Octet object" do
+    subject.must_be_kind_of(HttpObjects::Parameters::BasicRules::Octet)
+  end
+end
+
+describe HttpObjects::GeneralHeaders::Date do
+  subject { HttpObjects::GeneralHeaders::Date.parse("Tue, 15 Nov 1994 08:12:31 GMT") }
+  it "should be DateTime object" do
+    subject.must_be_kind_of(HttpObjects::Parameters::DateTime)
+  end
+end
+
+describe Trailer do
+  subject { Trailer.parse("") }
+  it "should be Octet object" do
+    subject.must_be_kind_of(HttpObjects::Parameters::BasicRules::Octet)
+  end
+end
+
+describe Upgrade do
+  subject { Upgrade.parse("HTTP/2.0, SHTTP/1.3, IRC/6.9, RTA/x11") }
+  it "should be Octet object" do
+    subject.must_be_kind_of(HttpObjects::Parameters::BasicRules::Octet)
+  end
+end
+
+describe Via do
+  subject { Via.parse("1.0 fred, 1.1 nowhere.com (Apache/1.1)") }
+  it "should be Octet object" do
+    subject.must_be_kind_of(HttpObjects::Parameters::BasicRules::Octet)
+  end
+end
+
+describe Warning do
+  subject { Warning.parse("") }
+  it "should be Octet object" do
+    subject.must_be_kind_of(HttpObjects::Parameters::BasicRules::Octet)
+  end
+end
