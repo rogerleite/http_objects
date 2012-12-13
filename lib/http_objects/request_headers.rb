0853f3c3ddf3991aec49a5da7f5857e57cd1f2d3
Roger Leite <roger.barreto@gmail.com>
2012-12-13 17:09:08 -0200

    HTTP Request headers If- pack

--- lib/http_objects/request_headers.rb
+++ lib/http_objects/request_headers.rb
@@ -43,4 +43,32 @@ module HttpObjects::RequestHeaders
   #    Host: www.w3.org
   Host = Header("Host", HttpObjects::Parameters::Uri)
 
+  # 14.24 If-Match
+  #    If-Match: "xyzzy"
+  #    If-Match: "xyzzy", "r2d2xxxx", "c3piozzzz"
+  #    If-Match: *
+  # Pending: implement spec
+  IfMatch = Header("If-Match", HttpObjects::Parameters::BasicRules::Token)
+
+  # 14.25 If-Modified-Since
+  #    If-Modified-Since: Sat, 29 Oct 1994 19:43:31 GMT
+  IfModifiedSince = Header("If-Modified-Since", HttpObjects::Parameters::DateTime)
+
+  # 14.26 If-None-Match
+  #    If-None-Match: "xyzzy"
+  #    If-None-Match: W/"xyzzy"
+  #    If-None-Match: "xyzzy", "r2d2xxxx", "c3piozzzz"
+  #    If-None-Match: W/"xyzzy", W/"r2d2xxxx", W/"c3piozzzz"
+  #    If-None-Match: *
+  # Pending: implement spec
+  IfNoneMatch = Header("If-None-Match", HttpObjects::Parameters::BasicRules::Token)
+
+  # 14.27 If-Range
+  # Pending: implement spec
+  IfRange = Header("If-Range", HttpObjects::Parameters::BasicRules::Token)
+
+  # 14.28 If-Unmodified-Since
+  #    If-Unmodified-Since: Sat, 29 Oct 1994 19:43:31 GMT
+  IfUnmodifiedSince = Header("If-Unmodified-Since", HttpObjects::Parameters::DateTime)
+
 end
---
+++ test/http_objects/request_headers/if_modified_since_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+include HttpObjects::RequestHeaders
+
+describe IfModifiedSince do
+
+  subject { IfModifiedSince.parse("Sat, 29 Oct 1994 19:43:31 GMT") }
+
+  it "should be Date Time object" do
+    subject.must_be_kind_of(HttpObjects::Parameters::DateTime)
+  end
+
+end
