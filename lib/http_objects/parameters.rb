f0dabb7be25954d2a186a4da44a3f8cbd98b7fcf
Roger Leite <roger.barreto@gmail.com>
2012-12-13 15:33:49 -0200

    Protocal Parameter module

---
+++ lib/http_objects/parameters.rb
@@ -0,0 +1,14 @@
+require "http_objects/parameters/basic_rules"
+require "http_objects/parameters/date_time"
+require "http_objects/parameters/media_type"
+require "http_objects/parameters/language_tags"
+require "http_objects/parameters/uri"
+
+module HttpObjects
+
+  # 3 Protocol Parameters
+  # This module have implementatios for Protocol Parameters from RFC 2616.
+  module Parameters
+  end
+
+end
